Fix pricing calculation

trader/farmer.py
@@ -54,7 +54,7 @@ class Farmer:
                 prices[good] = base_prices[good]
             else:
                 prices[good] = round(base_prices[good] * np.clip((
-                    base_abundances[good] / (0.1 + self.inventory[good]))**self.supply_sensitivity, 0.5, 2), 2)
+                    base_abundances[good] / max(0.1, self.inventory[good]))**self.supply_sensitivity, 0.5, 2), 2)
         return prices
 
     def init(self) -> None:

trader/location.py
@@ -47,7 +47,7 @@ class Location:
         base_abundances = {good: good.base_abundance for good in self.goods}
         prices = {
             good: round(base_prices[good] * np.clip((
-                    base_abundances[good] / (0.1 + self.supply_scores[good]))**self.supply_sensitivity, 0.25, 4), 2)
+                    base_abundances[good] / max(0.1, self.supply_scores[good]))**self.supply_sensitivity, 0.25, 4), 2)
             for good in self.goods}
         return prices
 